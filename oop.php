3fc57c7d7feb9c5e9ccfe746cf0ff2cebf143279
Shubhradeb Singha Roy <140373618+Shubhradeb1@users.noreply.github.com>
2024-05-17 17:46:12 +0600

    Add files via upload

--- oop.php
+++ oop.php
@@ -1,11 +1,98 @@
 <?php
-spl_autoload_register(function ($class_name) {
-    $file_path = "C:\Users\Shovon\OneDrive\Desktop\Php" . $class_name . ".php";
-    if (file_exists($file_path)) {
-        include $file_path;
-    } else {
-        echo "Class not found: $class_name\n";
-    }
-});
 
-$e = new oop();
+class Book {
+ // TODO: Add properties as Private
+    private $title;
+    private $availableCopies;
+ 
+ public function __construct($title, $availableCopies) {
+     // TODO: Initialize properties
+        $this->title = $title;
+        $this->availableCopies = $availableCopies;
+     }
+
+
+// TODO: Add getTitle method
+ public function getTitle()
+  {
+    return $this->title;
+  }
+
+
+ 
+
+
+
+ // TODO: Add getAvailableCopies method
+ public function getAvailableCopies() {
+        return $this->availableCopies;
+    }
+
+
+
+ // TODO: Add borrowBook method
+ public function borrowBook()
+  {
+    if ($this->availableCopies > 0) {
+      $this->availableCopies--;
+      echo "Book '{$this->title}' borrowed successfully.\n";
+    } else {
+      echo "Sorry, '{$this->title}' is not available.\n";
+    }
+  }
+
+
+
+ // TODO: Add returnBook method
+   public function returnBook()
+  {
+    $this->availableCopies++;
+    echo "Book '{$this->title}' returned. Thank you!\n";
+  }}
+
+
+
+class Member {
+ // TODO: Add properties as Private
+private $name;
+
+
+ public function __construct($name) {
+      // TODO: Initialize properties
+      $this->name = $name;
+     }
+
+
+
+ // TODO: Add getName method
+ public function getName() {
+        return $this->name;
+    }
+
+ 
+ // TODO: Add borrowBook method
+    public function borrowBook(Book $book)
+    {
+        return $book->borrowBook();
+    }
+ 
+
+
+
+ // TODO: Add returnBook method
+    public function returnBook(Book $book)
+    {
+        $book->returnBook();
+    }
+}
+
+$book1 = new Book("The Great Gatsby", 5);
+$book2 = new Book("To Kill a Mockingbird", 3);
+
+// Create 2 members
+$member1 = new Member("John Doe");
+$member2 = new Member("Jane Smith");
+
+// Members borrow books
+echo "Available Copies of '{$book1->getTitle()}': {$book1->getAvailableCopies()}\n";
+echo "Available Copies of '{$book2->getTitle()}': {$book2->getAvailableCopies()}\n";
